c38eeb7a84a0bb4da5fc048de7617b9f8bcef769
Tamal Saha <tamal@appscode.com>
2018-09-05 13:46:15 -0700

    Fix gh-tool path

--- hack/scripts/collect-github-star-report.sh
+++ hack/scripts/collect-github-star-report.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-gh-tools star-report --report-dir=/home/tamal/go/src/github.com/tamalsaha/star-report
 pushd /home/tamal/go/src/github.com/tamalsaha/star-report
+
+/home/tamal/go/bin/gh-tools star-report --report-dir=/home/tamal/go/src/github.com/tamalsaha/star-report
 git add --all
 git commit -a -s -m report\ $(date --iso-8601=seconds) || true
 git push origin master
+
 popd
