2457e752b174cd4f537d8118f7269f78a36db898
Tamal Saha <tamal@appscode.com>
2019-11-19 00:45:50 -0800

    Update dependencies

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/appscodelabs/gh-tools
 go 1.12
 
 require (
-	github.com/apex/log v1.1.0
-	github.com/appscode/go v0.0.0-20190424183524-60025f1135c9
+	github.com/apex/log v1.1.1
+	github.com/appscode/go v0.0.0-20191118114503-b0dd5d8305d9
 	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/google/go-github/v25 v25.0.4
 	github.com/jpillora/go-ogle-analytics v0.0.0-20161213085824-14b04e0594ef
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 )
--- vendor/github.com/appscode/go/version/version.go
+++ vendor/github.com/appscode/go/version/version.go
@@ -2,48 +2,51 @@ package version
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
+	semver "gomodules.xyz/version"
 )
 
 type version struct {
 	Version         string `json:"version,omitempty"`
 	VersionStrategy string `json:"versionStrategy,omitempty"`
-	Os              string `json:"os,omitempty"`
-	Arch            string `json:"arch,omitempty"`
 	CommitHash      string `json:"commitHash,omitempty"`
 	GitBranch       string `json:"gitBranch,omitempty"`
 	GitTag          string `json:"gitTag,omitempty"`
 	CommitTimestamp string `json:"commitTimestamp,omitempty"`
-	BuildTimestamp  string `json:"buildTimestamp,omitempty"`
-	BuildHost       string `json:"buildHost,omitempty"`
-	BuildHostOs     string `json:"buildHostOs,omitempty"`
-	BuildHostArch   string `json:"buildHostArch,omitempty"`
+	GoVersion       string `json:"goVersion,omitempty"`
+	Compiler        string `json:"compiler,omitempty"`
+	Platform        string `json:"platform,omitempty"`
+	// Deprecated
+	Os string `json:"os,omitempty"`
+	// Deprecated
+	Arch string `json:"arch,omitempty"`
+	// Deprecated
+	BuildTimestamp string `json:"buildTimestamp,omitempty"`
+	// Deprecated
+	BuildHost string `json:"buildHost,omitempty"`
+	// Deprecated
+	BuildHostOs string `json:"buildHostOs,omitempty"`
+	// Deprecated
+	BuildHostArch string `json:"buildHostArch,omitempty"`
 }
 
 func (v *version) Print() {
 	fmt.Printf("Version = %v\n", v.Version)
 	fmt.Printf("VersionStrategy = %v\n", v.VersionStrategy)
-	fmt.Printf("Os = %v\n", v.Os)
-	fmt.Printf("Arch = %v\n", v.Arch)
-
-	fmt.Printf("CommitHash = %v\n", v.CommitHash)
-	fmt.Printf("GitBranch = %v\n", v.GitBranch)
 	fmt.Printf("GitTag = %v\n", v.GitTag)
+	fmt.Printf("GitBranch = %v\n", v.GitBranch)
+	fmt.Printf("CommitHash = %v\n", v.CommitHash)
 	fmt.Printf("CommitTimestamp = %v\n", v.CommitTimestamp)
 
-	if v.BuildTimestamp != "" {
-		fmt.Printf("BuildTimestamp = %v\n", v.BuildTimestamp)
+	if v.GoVersion != "" {
+		fmt.Printf("GoVersion = %v\n", v.GoVersion)
 	}
-	if v.BuildHost != "" {
-		fmt.Printf("BuildHost = %v\n", v.BuildHost)
+	if v.Compiler != "" {
+		fmt.Printf("Compiler = %v\n", v.Compiler)
 	}
-	if v.BuildHostOs != "" {
-		fmt.Printf("BuildHostOs = %v\n", v.BuildHostOs)
-	}
-	if v.BuildHostArch != "" {
-		fmt.Printf("BuildHostArch = %v\n", v.BuildHostArch)
+	if v.Platform != "" {
+		fmt.Printf("Platform = %v\n", v.Platform)
 	}
 }
 
@@ -51,18 +54,34 @@ var Version version
 
 func NewCmdVersion() *cobra.Command {
 	var short bool
+	var check string
 	cmd := &cobra.Command{
 		Use:               "version",
 		Short:             "Prints binary version number.",
 		DisableAutoGenTag: true,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			if short {
 				fmt.Print(Version.Version)
-				os.Exit(0)
+			} else {
+				Version.Print()
 			}
-			Version.Print()
+			if check != "" {
+				c, err := semver.NewConstraint(check)
+				if err != nil {
+					return fmt.Errorf("failed to parse --check: %v", err)
+				}
+				v, err := semver.NewSemver(Version.Version)
+				if err != nil {
+					return fmt.Errorf("failed to parse version: %v", err)
+				}
+				if !c.Check(v) {
+					return fmt.Errorf("version %q fails to meet constraint %q", v.String(), c.String())
+				}
+			}
+			return nil
 		},
 	}
 	cmd.Flags().BoolVar(&short, "short", false, "Print just the version number.")
+	cmd.Flags().StringVar(&check, "check", "", "Check version constraint")
 	return cmd
 }
