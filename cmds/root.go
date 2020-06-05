d638a83932fb91ad1515bcdb1af8c3e3a38a939a
Tamal Saha <tamal@appscode.com>
2020-06-04 19:34:30 -0700

    Add Makefile
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/root.go
+++ cmds/root.go
@@ -1,24 +1,31 @@
+/*
+Copyright AppsCode Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmds
 
 import (
 	"flag"
 	"log"
-	"strings"
 
 	v "github.com/appscode/go/version"
-	"github.com/jpillora/go-ogle-analytics"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
-const (
-	gaTrackingCode = "UA-62096468-20"
-)
-
 func NewRootCmd(version string) *cobra.Command {
-	var (
-		enableAnalytics = true
-	)
 	cmd := &cobra.Command{
 		Use:               "gh-tools [command]",
 		Short:             `GitHub tools by AppsCode`,
@@ -27,18 +34,11 @@ func NewRootCmd(version string) *cobra.Command {
 			c.Flags().VisitAll(func(flag *pflag.Flag) {
 				log.Printf("FLAG: --%s=%q", flag.Name, flag.Value)
 			})
-			if enableAnalytics && gaTrackingCode != "" {
-				if client, err := ga.NewClient(gaTrackingCode); err == nil {
-					parts := strings.Split(c.CommandPath(), " ")
-					client.Send(ga.NewEvent(parts[0], strings.Join(parts[1:], "/")).Label(version))
-				}
-			}
 		},
 	}
 	cmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
-	flag.CommandLine.Parse([]string{})
-	cmd.PersistentFlags().BoolVar(&enableAnalytics, "analytics", enableAnalytics, "Send analytical events to Google Guard")
+	_ = flag.CommandLine.Parse([]string{})
 
 	cmd.AddCommand(NewCmdProtect())
 	cmd.AddCommand(NewCmdStarReport())
