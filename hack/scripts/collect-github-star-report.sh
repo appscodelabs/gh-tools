310461dd31b07fda7117ec468657919967da2492
1gtm <1gtm@appscode.com>
2020-09-23 04:42:16 +0000

    Update repository config
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- hack/scripts/collect-github-star-report.sh
+++ hack/scripts/collect-github-star-report.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright AppsCode Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 pushd /home/tamal/go/src/github.com/tamalsaha/star-report
 
 /home/tamal/go/bin/gh-tools star-report --report-dir=/home/tamal/go/src/github.com/tamalsaha/star-report
