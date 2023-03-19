e340ca17c4fdc1b677645bac4ee4fff361924dce
Tamal Saha <tamal@appscode.com>
2023-03-19 11:10:46 -0700

    Use github client v50
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/copy_releases.go
+++ cmds/copy_releases.go
@@ -28,7 +28,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v45/github"
+	"github.com/google/go-github/v50/github"
 	"github.com/spf13/cobra"
 	"golang.org/x/oauth2"
 	"gomodules.xyz/flags"
